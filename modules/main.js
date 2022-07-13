ec0a290648818ae517ca01dda83cbb749eab6843
rajnish <krajnish114@gmail.com>
2022-07-13 15:29:03 +0530

    putting module folder

---
+++ modules/main.js
@@ -0,0 +1,19 @@
+// import {default as rajnish} from './library.js';
+import rajnish from './library.js';
+import { message, user} from "./library.js";
+
+rajnish();
+user()
+console.log(message);
+
+
+// import { message, user as us, test } from "./library.js";
+// import * as rajnish from "./library.js";
+// console.log(rajnish.message);
+
+// document.body.innerHTML = rajnish.message;
+
+// // user('Amit Kumar');
+// console.log(rajnish.user('Amit Kumar'));
+
+// let a = new rajnish.test();
